447ec7e0dff9938f6d687dfbb1210d276432ab70
Hazar Belge <hazarbelge@gmail.com>
2021-10-17 02:36:15 +0300

    Project init

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app" class="bg-black min-h-screen">
+    <div class="bg-purple-900 shadow-md mb-4">
+      <div class="container mx-auto">
+        <nav class="flex items-center justify-between flex-wrap p-3">
+          <div class="flex justify-around">
+            <img
+              src="https://pbs.twimg.com/profile_images/1243623122089041920/gVZIvphd_400x400.jpg"
+              width="50"
+              height="50"
+              alt="themoviedb_logo"
+            />
+            <div
+              class="
+                pl-4
+                flex
+                w-full
+                sm:w-auto
+                mb-2
+                items-center
+                flex-shrink-0
+                text-white
+              "
+            >
+              <span class="font-bold text-xl tracking-tight m-auto"
+                >The Movie DB API - VueJS</span
+              >
+            </div>
+          </div>
+          <div class="flex w-1/2 sm:w-auto justify-center">
+            <router-link to="/" class="hover:text-pink-400 text-white p-2" exact
+              >Home
+            </router-link>
+            <router-link
+              to="/categories"
+              class="hover:text-pink-400 text-white p-2"
+              exact
+              >Movies
+            </router-link>
+          </div>
+          <div class="flex w-1/2 sm:w-auto justify-center">
+            <select
+              @change="updateCurrentLanguage($event)"
+              class="
+                text-center
+                appearance-none
+                bg-gray-200
+                border border-pink-300
+                hover:bg-pink-100
+                text-gray-700
+                p-1
+                rounded
+                focus:outline-none focus:bg-gray-200 focus:border-gray-500
+              "
+            >
+              <option
+                v-for="(language, index) in languageList"
+                :value="language.value"
+                :key="index"
+                :selected="language.value === languageCurrent.value"
+              >
+                {{ language.label }}
+              </option>
+            </select>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <div class="container mx-auto px-3 pb-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "App",
+  computed: {
+    ...mapGetters(["languageCurrent", "languageList"]),
+  },
+  methods: {
+    ...mapActions({
+      changeCurrentLanguage: "changeCurrentLanguage",
+      fetchLanguages: "fetchLanguages",
+    }),
+    updateCurrentLanguage(event) {
+      this.changeCurrentLanguage(event.target.value);
+    },
+  },
+  created() {
+    this.fetchLanguages();
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { charactersLimiter, imdbLink } from "./filter";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import VueClazyLoad from "vue-clazy-load";
+
+Vue.use(VueClazyLoad);
+
+library.add(fas);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+import "@/assets/css/tailwind.css";
+
+Vue.config.productionTip = false;
+
+import axios from "axios";
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+
+Vue.filter("charactersLimiter", charactersLimiter);
+Vue.filter("imdbLink", imdbLink);
+
+new Vue({
+  router,
+  store,
+  render: (render) => render(App),
+}).$mount("#app");
